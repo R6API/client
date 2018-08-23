358fc57b7f0d66e687c9b0b88043d2aa2c3b6c4d
Baptiste Leduc <baptiste@weglot.com>
2018-08-23 10:50:00 +0200

    adding default profileIds to ApiTestCase

--- tests/Api/ApiTestCase.php
+++ tests/Api/ApiTestCase.php
@@ -19,6 +19,15 @@ abstract class ApiTestCase extends TestCase
     /** @var ClientInterface */
     protected $client;
 
+    /** @var array */
+    protected $profileIds = [
+        '71b29fd6-3c4d-46af-96fe-898960ef3faf', // Manlyfique
+        '2a60ef4d-6ee1-4cc0-ad4d-1c0d8c964763', // CPC.Evene
+        'da8d33f8-1dbe-4efc-a2e4-a0a1e0c68108', // Shep_FR
+        '575b8c76-a33a-4c19-9618-d14b9343d527', // panda________
+        'fe50c151-d5f4-451b-ac17-33e0b2d0a2fb' // Malotru_
+    ];
+
     public function setUp()
     {
         $cacheItemPool = new PredisCachePool(new Client(getenv('REDIS_URI')));
